9424b5113088241577920855e8282e5a527b3485
can_can_need <51284550+stainzhao@users.noreply.github.com>
2026-08-17 11:26:56 +0800

    bump client to 7.15.1

--- p2p/wireguard-p2p-client/main.go
+++ p2p/wireguard-p2p-client/main.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	version          = "7.15.0"
+	version          = "7.15.1"
 	apiBase          = "http://10.0.0.1:8899"
 	keepalive        = 25
 	onlineMaxAge     = 3 * time.Minute
